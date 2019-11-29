e63530551ee33a09dbfde9cf4d18cabdd16e2965
Firas Qutishat <firas.qutishat@securekey.com>
2019-11-29 14:42:52 -0500

    chore: Rename in invitation to CreateInvitation
    closes #6
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- components/invitation/CreateInvitation.stories.js
+++ components/invitation/CreateInvitation.stories.js
@@ -4,14 +4,14 @@ Copyright SecureKey Technologies Inc. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 import { storiesOf } from '@storybook/vue'
-import invitation from './invitation'
+import createInvitation from './CreateInvitation'
 
 
 storiesOf('invitation', module)
-    .add('normal', () => ({
-        components: { invitation },
+    .add('create', () => ({
+        components: { createInvitation },
         template:
             `
-      <invitation/>
+      <createInvitation/>
     `
     }))
--- components/invitation/index.js
+++ components/invitation/index.js
@@ -3,4 +3,13 @@ Copyright SecureKey Technologies Inc. All Rights Reserved.
 
 SPDX-License-Identifier: Apache-2.0
 */
-export * from './invitation.vue'
+
+import CreateInvitation from "./CreateInvitation.vue";
+
+export default {
+    install(Vue, options) {
+        // Let's register our component globally
+        // https://vuejs.org/v2/guide/components-registration.html
+        Vue.component("create-invitation", CreateInvitation);
+    }
+};
